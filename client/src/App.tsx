d1019e8cd12c8475a1919e6a218f153a8fa83e82
Aditya <adi22feb@gmail.com>
2024-06-04 02:06:21 +0530

    Added linkButtonGen page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Signup from "./Signup";
 import LandingPage from "./LandingPage";
 import Login from "./Login";
+import LinkButtonGenPage from "./LinkButtonGenPage";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/register" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/:username" element={<LinkButtonGenPage />} />
       </Routes>
     </Router>
   );
---
+++ client/src/LinkButtonGenPage.tsx
@@ -0,0 +1,9 @@
+const LinkButtonGenPage = () => {
+  return (
+    <div className="bg-secondary vh-100">
+      <button className="btn btn-success p-100">Add a new link</button>
+    </div>
+  );
+};
+
+export default LinkButtonGenPage;
